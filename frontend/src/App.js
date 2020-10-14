e269dac7cc67420b9702cc5d931ab78fad6a6215
Ross Young <rossmichaelyoung@gmail.com>
2020-10-14 14:26:31 -0500

    Frontend Structure

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import { Home } from "./pages/Home";
+import Find from "./pages/Find";
+import Create from "./pages/Create";
+import { About } from "./pages/About";
+
+export const App = () => {
+  return (
+    <main>
+      <Switch>
+        <Route path='/' component={Home} exact />
+        <Route path='/find' component={Find} exact />
+        <Route path='/create' component={Create} exact />
+        <Route path='/about' component={About} exact />
+        <Route component={Error} />
+      </Switch>
+    </main>
+  );
+};
