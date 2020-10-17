671ccf80568b4d2632459019db173bb36822ae0e
Edgar Portales <edgar_ivanhdz15@hotmail.com>
2020-10-17 16:26:52 -0500

    Submit games, and get games routes created

--- backend/flaskServer.py
+++ backend/flaskServer.py
@@ -1,15 +1,18 @@
-from flask import Flask
+from flask import Flask, request
 from .Modules import PostgresConnector
+
 def create_app():
     database_conn = PostgresConnector()
     app = Flask(__name__)
 
 
-    @app.route('/games/submit')
+    @app.route('/games/submit', methods=['GET', 'POST'])
     def submit_games():
+        game = request.get_json()
+        print(game)
         return 'Hello, World!'
 
-    @app.route('/games/get')
+    @app.route('/games/get',  methods=['GET', 'POST'])
     def get_games():
         return "Hi there"
 
