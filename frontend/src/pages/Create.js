29f338d1c9bc1406d87daad5df32b3a8cd753d47
Ross Young <rossmichaelyoung@gmail.com>
2020-10-19 12:32:30 -0500

    Display Games Dynamically in Grid of Card Components and Create Games intake form

--- frontend/src/pages/Create.js
+++ frontend/src/pages/Create.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
 import { PageContainer } from "../components/PageContainer";
+import { Formik } from "formik";
+import { Input, Button, Tag } from "antd";
+
+const theme = {
+  margin: "0 auto",
+  textAlign: "center",
+};
 
 class Create extends Component {
   constructor(props) {
@@ -9,7 +16,139 @@ class Create extends Component {
   }
 
   generateContent = () => {
-    return <h2 style={{ color: "white" }}>Create Game</h2>;
+    const tagStyle = { backgroundColor: "black", color: "white" };
+    const divStyle = { marginTop: "2%", marginBottom: "0%" };
+    return (
+      <div style={{ ...theme }}>
+        <Formik
+          initialValues={{
+            "game-name": "",
+            description: "",
+            "media-name": "",
+            "media-type": "",
+            players: "",
+          }}
+          validate={(values) => {
+            const errors = {};
+            if (!values["game-name"]) {
+              errors["game-name"] = "Game Name Required";
+            }
+
+            if (!values["description"]) {
+              errors["description"] = "Description Required";
+            }
+
+            if (!values["media-name"]) {
+              errors["media-name"] = "Media Name Required";
+            }
+
+            if (!values["media-type"]) {
+              errors["media-type"] = "Media Type Required";
+            }
+
+            if (!values["players"]) {
+              errors["players"] = "Number of Players Required";
+            }
+
+            return errors;
+          }}
+          onSubmit={(game) => {
+            console.log(game);
+          }}>
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+            submitForm,
+            isValid,
+            /* and other goodies */
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <div style={divStyle}>
+                <Input
+                  type='game-name'
+                  name='game-name'
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values["game-name"]}
+                  placeholder='Game Name'
+                />
+              </div>
+              {errors["game-name"] && touched["game-name"] && (
+                <Tag style={tagStyle}>{errors["game-name"]}</Tag>
+              )}
+              <div style={divStyle}>
+                <Input
+                  type='description'
+                  name='description'
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values["description"]}
+                  placeholder='Game Description'
+                />
+              </div>
+              {errors["description"] && touched["description"] && (
+                <Tag style={tagStyle}>{errors["description"]}</Tag>
+              )}
+              <div style={divStyle}>
+                <Input
+                  type='media-name'
+                  name='media-name'
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values["media-name"]}
+                  placeholder='Media Name'
+                />
+              </div>
+              {errors["media-name"] && touched["media-name"] && (
+                <Tag style={tagStyle}>{errors["media-name"]}</Tag>
+              )}
+              <div style={divStyle}>
+                <Input
+                  type='media-type'
+                  name='media-type'
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values["media-type"]}
+                  placeholder='Media Type'
+                />
+              </div>
+              {errors["media-type"] && touched["media-type"] && (
+                <Tag style={tagStyle}>{errors["media-type"]}</Tag>
+              )}
+              <div style={divStyle}>
+                <Input
+                  type='players'
+                  name='players'
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values["players"]}
+                  placeholder='Number of Players'
+                />
+              </div>
+              {errors["players"] && touched["players"] && (
+                <Tag style={tagStyle}>{errors["players"]}</Tag>
+              )}
+              <div style={divStyle}>
+                <Button
+                  onClick={() => {
+                    submitForm();
+                  }}
+                  type='primary'
+                  block
+                  disabled={isSubmitting || (touched && !isValid)}>
+                  Submit Game
+                </Button>
+              </div>
+            </form>
+          )}
+        </Formik>
+      </div>
+    );
   };
 
   render() {
--- frontend/src/pages/Find.js
+++ frontend/src/pages/Find.js
@@ -1,15 +1,25 @@
 import React, { Component } from "react";
 import { PageContainer } from "../components/PageContainer";
 import { getAllGames } from "../utils/routes";
-import styles from "../App.css";
-import "../App.css";
+import Card from "@material-ui/core/Card";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import Typography from "@material-ui/core/Typography";
+import CardContent from "@material-ui/core/CardContent";
+import "../css/App.css";
+
+const theme = {
+  margin: "0 auto",
+  textAlign: "center",
+  color: "black",
+};
 
 class Find extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      content: "",
+      games: [],
     };
   }
 
@@ -23,29 +33,73 @@ class Find extends Component {
 
   getGames = () => {
     getAllGames()
-      .then((res) => res.text())
+      .then((res) => res.json())
       .then((resp) => {
         console.log(resp);
-        this.updateState("content", resp);
+        if (resp.Response === 200) {
+          this.updateState("games", resp.games);
+        }
       });
   };
 
-  generateContent = () =>
-  {
+  generateContent = () => {
+    const { games } = this.state;
     return (
-      <div>
+      <>
+        <div>
+          {/* This font is imported from google fonts. The css file it is imported in is App.css. */}
+          <h2
+            style={{
+              fontSize: "75px",
+              fontFamily: "Bungee Shade, cursive",
+              color: "#F2CA80",
+              textAlign: "center",
+            }}>
+            Fill My Glass
+          </h2>
 
-        {/* This font is imported from google fonts. The css file it is imported in is App.css. */}
-        <h2 style={{fontSize: "75px", fontFamily: "Bungee Shade, cursive", color: "#F2CA80", textAlign: "center"}}>{this.state.content}</h2>
+          {/*  This "form" element is not implemented yet. It is just a placeholder. */}
+          <form style={{ textAlign: "center" }}>
+            <p style={{ color: "white" }}> Search Drinking Games </p>
+            <input type='text' style={{ width: "300px" }} />
+            <input type='button' onclick='SearchForThis' value='Search' />
+          </form>
+        </div>
 
-        {/*  This "form" element is not implemented yet. It is just a placeholder. */}
-        <form style={{textAlign: "center"}}>
-          <p style ={{color: "white"}}> Search Drinking Games </p>
-          <input type="text" style={{width: "300px"}}/>
-          <input type="button" onclick="SearchForThis" value="Search"/>
-        </form>
-        
-      </div>
+        <div style={{ margin: "5% 10%" }}>
+          <GridList
+            cellHeight={160}
+            cols={3}
+            className={{
+              width: 500,
+              height: 450,
+            }}>
+            {games.map((game, index) => (
+              <GridListTile key={index} cols={1}>
+                <Card>
+                  <CardContent>
+                    <Typography style={{ ...theme }}>
+                      {game["game-name"]}
+                    </Typography>
+                    <Typography style={{ ...theme }}>
+                      {game["description"]}
+                    </Typography>
+                    <Typography style={{ ...theme }}>
+                      {game["media-name"]}
+                    </Typography>
+                    <Typography style={{ ...theme }}>
+                      {game["media-type"]}
+                    </Typography>
+                    <Typography style={{ ...theme }}>
+                      {game["players"]}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </GridListTile>
+            ))}
+          </GridList>
+        </div>
+      </>
     );
   };
 
