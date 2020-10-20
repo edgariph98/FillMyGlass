7e8516f457df3950ffefb69ca336881e8c1560a3
English-Ghost <56802968+English-Ghost@users.noreply.github.com>
2020-10-20 18:57:27 -0500

    changed home page
    updated some buttons and fixed the scrolling on the front page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,13 +2,15 @@ import React from "react";
 import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
 import HomePageContainer from "../components/HomePageContainer";
+import "../css/BasicStyles.css";
 
 export const Home = () => (
   <HomePageContainer>
     <div>
+      <h4>Hello.</h4>
       <h2>Are You 21+?</h2>
-      <Button variant='contained'>
-        <Link to='/find'>Yes</Link>
+      <Button class="oval-button" variant='contained' href='/find'>
+        Yes
       </Button>
     </div>
   </HomePageContainer>
