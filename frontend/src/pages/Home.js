69ac23e11dbc2699d167b661a8f97bfff0b53e70
English-Ghost <56802968+English-Ghost@users.noreply.github.com>
2020-11-18 19:29:44 -0600

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -7,7 +7,9 @@ import "../css/BasicStyles.css";
 export const Home = () => (
   <HomePageContainer>
     <div>
-      <h4>Hello.</h4>
+      <h4>You must be of legal drinking age to access this Site. <br></br>
+      Fill My Cup does not sell or serve alcohol to persons under the age of 21.<br></br>
+      By accessing Fill My Cup Site, you swear and affirm that you are over the age of 21.</h4>
       <h2>Are You 21+?</h2>
       <Button class="oval-button" variant='contained' href='/find'>
         Yes
